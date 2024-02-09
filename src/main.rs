1384e92d778395bb5e2c51a62a6dfca8491a4f41
Dimas Leenman <skythedragon@outlook.com>
2024-02-09 19:52:27 +0100

    move stuff

--- src/main.rs
+++ src/main.rs
@@ -16,18 +16,20 @@
 // assets are NOT deduplicated, and are inserted in the page directory
 // static assets can be loaded with static
 
+mod filetree;
+
 use std::{
     collections::HashMap,
-    env::{self, current_dir, set_current_dir},
+    env::{current_dir, set_current_dir},
     ffi::OsString,
-    fmt::Pointer,
-    fs::{read_dir, read_to_string, DirEntry, File},
-    path::{Path, PathBuf},
-    str::FromStr,
+    fs::read_to_string,
+    path::PathBuf,
 };
 
 use clap::Parser;
-use mlua::{AsChunk, Table};
+use mlua::Lua;
+
+use crate::filetree::load_tree;
 
 /// A single item in the page tree
 #[derive(Debug)]
@@ -50,23 +52,6 @@ enum SiteNode {
     Dir { subs: HashMap<OsString, SiteNode> },
 }
 
-/// A single item in the file tree
-#[derive(Debug)]
-enum FileNode {
-    /// A lua file to process, as well as all of it's siblings
-    Lua {
-        code: String,
-        name: OsString,
-        sibs: HashMap<OsString, FileNode>,
-    },
-
-    /// An asset file
-    Asset { path: PathBuf },
-
-    /// A directory
-    Dir { subs: HashMap<OsString, FileNode> },
-}
-
 #[derive(Parser)]
 struct Args {
     /// directory to the site.lua file of the site to build, current working directory by default
@@ -76,77 +61,6 @@ struct Args {
     /// directory to write to, public/ by default
     #[clap(short, long)]
     output: Option<PathBuf>,
-}
-
-// load the file tree
-fn load_tree<P: AsRef<Path>>(root: P) -> Option<FileNode> {
-    // find all the lua files
-    let luafiles = read_dir(&root)
-        .expect("Failed to read directory!")
-        .filter_map(|x| x.ok())
-        .filter(|x| x.file_type().expect("Failed to get file type").is_file())
-        .filter(|x| x.file_name() != "index.lua")
-        .filter(|x| x.path().extension().map(|x| x == "lua").unwrap_or(false));
-
-    // find all the other files
-    let assetfiles = read_dir(&root)
-        .expect("Failed to read directory!")
-        .filter_map(|x| x.ok())
-        .filter(|x| x.file_type().expect("Failed to get file type").is_file())
-        .filter(|x| x.path().extension().map(|x| x != "lua").unwrap_or(true));
-
-    // find all the subdirectories
-    let subdirs = read_dir(&root)
-        .expect("Failed to read directory!")
-        .filter_map(|x| x.ok())
-        .filter(|x| x.file_type().expect("Failed to get file type").is_dir());
-
-    // load the assets
-    let siblings = assetfiles.map(|x| (x.file_name(), FileNode::Asset { path: x.path() }));
-
-    // load any lua files
-    let scripts = luafiles.map(|x| {
-        let code = read_to_string(x.path()).unwrap();
-        (
-            x.file_name(),
-            FileNode::Lua {
-                code,
-                name: x
-                    .path()
-                    .file_stem()
-                    .map(|x| x.to_os_string())
-                    .unwrap_or(x.file_name().to_os_string()),
-                sibs: HashMap::new(),
-            },
-        )
-    });
-
-    // create the subdirs, as well as the lua files and assets
-    let subs = subdirs
-        .map(|x| {
-            let node = load_tree(x.path()).expect("Failed to load subtree!");
-            (x.file_name(), node)
-        })
-        .chain(scripts)
-        .chain(siblings)
-        .collect::<HashMap<OsString, FileNode>>();
-
-    // if there is an index.lua, load it and return this as a lua file
-    if let Ok(code) = read_to_string(root.as_ref().join("index.lua")) {
-        Some(FileNode::Lua {
-            code,
-            name: root
-                .as_ref()
-                .file_name()
-                .map(|x| x.to_os_string())
-                .unwrap_or(OsString::from("index.lua")),
-            sibs: subs,
-        })
-
-    // if not, this is a directory
-    } else {
-        Some(FileNode::Dir { subs })
-    }
 }
 
 fn main() {
@@ -179,6 +93,11 @@ fn main() {
 
     // load the tree
     let filetree = load_tree("content/");
+
+    // load and convert the sass styles
+
+    // start lua
+    let lua = Lua::new();
 
     println!("{:?}", filetree);
 }
