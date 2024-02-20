5eddad9a5a0f79434b43514aa00f206f6d08e0a0
Dimas Leenman <skythedragon@outlook.com>
2024-02-20 16:56:53 +0100

    multiple commands

--- src/main.rs
+++ src/main.rs
@@ -6,22 +6,46 @@ use cmd::generate::Site;
 use std::path::PathBuf;
 
 #[derive(Parser)]
-struct Args {
-    /// directory to the site.lua file of the site to build, current working directory by default
-    #[clap(short, long)]
-    dir: Option<PathBuf>,
+enum Args {
+    /// Build the site
+    Build {
+        /// directory to the site.toml file of the site to build, current working directory by default
+        #[clap(short, long)]
+        dir: Option<PathBuf>,
 
-    /// directory to write to, public/ by default
-    #[clap(short, long)]
-    output: Option<PathBuf>,
+        /// directory to write to, public/ by default
+        #[clap(short, long)]
+        output: Option<PathBuf>,
+    },
+
+    /// Serve the site locally, for development
+    Dev {
+        /// directory to the site.toml file of the site to build, current working directory by default
+        #[clap(short, long)]
+        dir: Option<PathBuf>,
+    },
+
+    /// List example scripts
+    Cookbook {},
 }
 
 fn main() -> Result<(), anyhow::Error> {
     let args = Args::parse();
 
-    let site = Site::generate(args.dir)?;
+    // run the command
+    match args {
+        Args::Build { dir, output } => {
+            let site = Site::generate(dir)?;
 
-    site.write_to_directory::<PathBuf>(None)?;
+            for warning in site.warnings.iter() {
+                println!("[WARN] {}", warning);
+            }
+
+            site.write_to_directory(output)?;
+        }
+        Args::Dev { dir } => {}
+        Args::Cookbook {} => {}
+    }
 
     Ok(())
 }
