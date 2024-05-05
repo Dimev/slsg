2d0f3a442ff8c68ef172cee8ba76d1686b71144a
Dimas Leenman <skythedragon@outlook.com>
2024-05-05 12:34:57 +0200

    grass and started on the highlighters again

--- src/generate.rs
+++ src/generate.rs
@@ -1,12 +1,21 @@
 use std::{cell::RefCell, collections::HashMap, rc::Rc};
 
+use anyhow::anyhow;
+use fancy_regex::Regex;
+use latex2mathml::{latex_to_mathml, DisplayStyle};
 use mlua::{Lua, LuaOptions, StdLib, Value};
 use std::{fs, path::Path};
 
 use crate::file::File;
 
+// rule for the highlighter
+enum HighlightRule {
+    Raw(String, String),
+    Compiled(String, Regex),
+}
+
 /// Generate the site from the given lua file
-pub fn generate(path: &Path) -> anyhow::Result<HashMap<String, File>> {
+pub fn generate(path: &Path, dev: bool) -> anyhow::Result<HashMap<String, File>> {
     // lua
     let lua = Lua::new_with(
         StdLib::COROUTINE | StdLib::TABLE | StdLib::STRING | StdLib::UTF8 | StdLib::MATH,
@@ -24,6 +33,9 @@ pub fn generate(path: &Path) -> anyhow::Result<HashMap<String, File>> {
 
     lua.globals().set("require", require)?;
 
+    // language highlighters
+    let highlighters = Rc::new(RefCell::new(HashMap::<String, Vec<HighlightRule>>::new()));
+
     // load our library functions
     let lib = lua.create_table()?;
 
@@ -37,6 +49,8 @@ pub fn generate(path: &Path) -> anyhow::Result<HashMap<String, File>> {
     // read file
     // TODO: file api(?)
 
+    // new file
+
     // read toml
     // read yaml
     // read json
@@ -44,16 +58,67 @@ pub fn generate(path: &Path) -> anyhow::Result<HashMap<String, File>> {
 
     // read and eval mdl
 
-    // add highlighter
+    // add highlighters
+    let highlighters_cloned = highlighters.clone();
+    let add_highlighters = lua.create_function(move |_, text: String| {
+        // parse the highlighter
+        let raw = toml::from_str::<HashMap<String, Vec<(String, String)>>>(&text)
+            .map_err(mlua::Error::external)?;
+        let mut highlight = highlighters_cloned.borrow_mut();
+
+        // add the language to the highlighters
+        for (key, value) in raw.into_iter() {
+            highlight.insert(
+                key,
+                value
+                    .into_iter()
+                    .map(|(rule, regex)| HighlightRule::Raw(rule, regex))
+                    .collect(),
+            );
+        }
+
+        Ok(())
+    })?;
 
     // highlight code
+    let highlighters_cloned = highlighters.clone();
+    let highlight_code = lua.create_function(move |_, (lang, code): (String, String)| {
+        // get the language
+        let rules = highlighters_cloned
+            .borrow()
+            .get(&lang)
+            .ok_or(mlua::Error::external(anyhow!(
+                "Language {lang} not in highlighters!"
+            )));
+
+        // highlight
+
+        Ok("sus mogus")
+    })?;
     // highlight code to ast
 
     // highlight latex math as mathml
+    let mathml = lua.create_function(|_, (text, inline): (String, Option<bool>)| {
+        latex_to_mathml(
+            &text,
+            if inline.unwrap_or(false) {
+                DisplayStyle::Inline
+            } else {
+                DisplayStyle::Block
+            },
+        )
+        .map_err(mlua::Error::external)
+    })?;
 
     // minify/bundle(?)
 
     // dev mode?
+    lib.set("dev", dev)?;
+
+    // add all to the site
+    lib.set("latex2Mathml", mathml)?;
+    lib.set("addHighlighters", add_highlighters)?;
+    lib.set("highlightCodeToHtml", highlight_code)?;
 
     lua.globals().set("site", lib)?;
 
