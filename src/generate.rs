2242f20903824d663ce5e5d5461802346c76b7df
Dimas Leenman <skythedragon@outlook.com>
2025-05-23 14:52:38 +0200

    ordering of folders when generating

--- src/generate.rs
+++ src/generate.rs
@@ -16,6 +16,18 @@ pub(crate) struct Site {
     pub not_found: Option<Vec<u8>>,
 }
 
+const INDEX_FILES: &[&str] = &[
+    "index.htm",
+    "index.html",
+    "index.lua.htm",
+    "index.fnl.htm",
+    "index.lua.html",
+    "index.fnl.html",
+    "index.mmk",
+    "index.lua.mmk",
+    "index.fnl.mmk",
+];
+
 /// Generate the site
 /// Assumes that the current directory contains the site.conf file
 pub(crate) fn generate() -> Result<Site> {
@@ -43,34 +55,80 @@ pub(crate) fn generate() -> Result<Site> {
         .context("failed to install fennel")?;
     }
 
-    // final files
-    let mut files = BTreeMap::new();
+    // files to process, in that order
+    let mut process = Vec::new();
 
     // depth first traversal of the directories
     let mut stack = vec![PathBuf::from(".")];
     while let Some(path) = stack.pop() {
-        println!("{path:?}");
         if path.is_dir() {
+            let mut dirs = Vec::new();
+            let mut files = Vec::new();
+            let mut index = None;
+
             // directory? recurse
             for path in fs::read_dir(path)
                 .into_lua_err()
                 .context("Could not read directory")?
             {
-                // add to the stack
-                stack.push(path?.path());
+                let path = path
+                    .into_lua_err()
+                    .context("Failed to read directory entry")?;
+
+                // take depending on type
+                if path
+                    .file_name()
+                    .to_str()
+                    .map(|x| INDEX_FILES.contains(&x))
+                    .unwrap_or(false)
+                {
+                    if index.is_none() {
+                        index = Some(path.path())
+                    } else {
+                        return Err(mlua::Error::external(format!(
+                            "Double index file found in directory `{:?}`",
+                            path
+                        )));
+                    }
+                } else if path
+                    .file_type()
+                    .into_lua_err()
+                    .context("Failed to get file type")?
+                    .is_file()
+                {
+                    files.push(path.path());
+                } else {
+                    dirs.push(path.path());
+                }
             }
+
+            // push all to stack
+            // stack, so this is reversed
+            // index last, if any, in order to build an index from all other files in the directory
+            stack.extend(index.into_iter());
+
+            // files go after directories
+            stack.extend(files.into_iter());
+
+            // directories first
+            stack.extend(dirs.into_iter());
         } else if path.is_file() {
-            // file? read
-            files.insert(
-                path.strip_prefix("./")
-                    .into_lua_err()
-                    .context("Expected a `./` at the start of the path")?
-                    .to_owned(),
-                fs::read(path)
-                    .into_lua_err()
-                    .context("Failed to read file")?,
-            );
+            process.push(path);
         }
+    }
+
+    println!("{:?}", process);
+
+    // final files
+    let mut files = BTreeMap::new();
+
+    for path in process {
+        files.insert(
+            path.clone(),
+            fs::read(path.clone())
+                .into_lua_err()
+                .context(format!("Failed to read `{:?}`", path))?,
+        );
     }
 
     // filter out all paths to ignore
@@ -84,3 +142,5 @@ pub(crate) fn generate() -> Result<Site> {
     })
     //todo!()
 }
+
+
